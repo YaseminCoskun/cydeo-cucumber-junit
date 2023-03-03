b4f0ae8040e56e7eb14fda3a304412439fb63842
Emine Yasemin Coskun <emineyasemincoskun@gmail.com>
2023-03-03 09:47:15 +0000

    Initial commit

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,27 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-reports.html" ,
+                "rerun:target/rerun.text",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features= "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = "@smoke",
+        publish= true
+)
+
+public class CukesRunner {
+
+
+}
+
+
